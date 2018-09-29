bacf12735caa274712ffa70297dd67a278f4e050
Ho Wa Wong <howa.wong@gmail.com>
2018-09-29 09:53:03 +0800

    Add create-react-app Hello World project

--- presentation/index.js
+++ presentation/index.js
@@ -4,6 +4,8 @@ import React from "react";
 // Import Spectacle Core tags
 import {
   BlockQuote,
+  Code,
+  CodePane,
   Cite,
   Deck,
   Heading,
@@ -36,35 +38,77 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+             Deploying web application with Docker
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Ho Wa Wong, howa (dot) wong (at) gmail (dot) com
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={6} textColor="primary" caps>Agenda</Heading>
+          <Heading size={6} textColor="secondary">Introduction to Docker</Heading>
+          <Heading size={6} textColor="secondary">Building a Docker image</Heading>
+          <Heading size={6} textColor="secondary">Running a Docker container locally</Heading>
+          <Heading size={6} textColor="secondary">Mimimize image size</Heading>
+          <Heading size={6} textColor="secondary">Deploying to heroku</Heading>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={6} textColor="secondary" caps>About myself</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Python Developer</ListItem>
+            <ListItem>Open Data Advocate</ListItem>
+            <ListItem>Focus on data and backend</ListItem>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Docker Introduction</Quote>
           </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>What is a container?</Heading>
+          <List>
+            <ListItem>A set of processes which are isolated from OS</ListItem>
+            <ListItem>But isolated CPU, memory, file system etc</ListItem>
+            <ListItem>Different from VM</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>Container Image</Heading>
+          <List>
+            <ListItem>Lightweight, standalone, executable package</ListItem>
+            <ListItem>It has application code / program, system libraries and configuration</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>Installation</Heading>
+          <List>
+            <ListItem>Go to https://docs.docker.com/install/</ListItem>
+            <ListItem>(Windows user might need to install VirtualBox or Hyper-V)</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Coding Time</Quote>
+            <br/> Let's write the very first Dockerfile
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+		  <CodePane
+            lang="jsx"
+            source={require('raw-loader!../assets/Dockerfile.example')}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide>
+          <Code>
+	 		docker build -t sitcon .
+	      </Code>
+        </Slide>
+        <Slide>
+           Nothing happens ?!
+           Of course, we need to run the container!
         </Slide>
       </Deck>
     );
